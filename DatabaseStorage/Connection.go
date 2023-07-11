c36a3414e51e46aa5fd91b4f1760b8f1c35899f3
Irina-Bunt <irinabuntina17@gmail.com>
2023-07-11 13:29:05 +0700

    Initial commit

---
+++ DatabaseStorage/Connection.go
@@ -0,0 +1,11 @@
+package DatabaseStorage
+
+var dbase *gorm.DB
+
+func Init() *gorm.DB {
+
+}
+
+func GetDB() *gorm.DB {
+
+}
---
+++ model/models.go
@@ -0,0 +1,6 @@
+package model
+
+type Tokens struct {
+	Id     uint
+	UserId uint
+}
